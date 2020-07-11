82df8d6cabcc28a41a2bf11fe4dbf00adc30f2d5
Navaxa <62459594+Navaxa@users.noreply.github.com>
2020-07-11 16:27:40 -0500

    Update Consultas.php

--- login/CRUD/class/Consultas.php
+++ login/CRUD/class/Consultas.php
@@ -7,7 +7,7 @@ class Usuarios
     private function __construct()
     {
 	    
-	    $serverName = "enerfy-server.database.windows.net\Navaxa"; //serverName\instanceName
+	    $serverName = "enerfy-server.database.windows.net\NAVAXA"; //serverName\instanceName
 $connectionInfo = array( "Database"=>"area-energetica", "UID"=>"nava-admin", "PWD"=>"Pass1234");
 $conn = sqlsrv_connect( $serverName, $connectionInfo);
 
